6e63c1fa8e560ab7ac8d18140416f5b1acf5c20f
Enku Ergetekal <enkuergetekal@gmail.com>
2023-02-19 10:58:44 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print all single digit od base 10 starting from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
