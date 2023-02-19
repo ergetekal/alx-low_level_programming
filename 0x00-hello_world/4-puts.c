e59888ba664c2f0789b215ead84b1bc56c7b1df5
Enku Ergetekal <enkuergetekal@gmail.com>
2023-02-19 02:39:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * mani - entry
- * Return: 0
+ * main - Entry piont
+ * Description: 'use puts'
+ * Return: Always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
